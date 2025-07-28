c317c467e793757072bb66c5343d7b2379a7ccac
forcepusher <bionitsoup@gmail.com>
2025-07-28 15:22:40 +0700

    Update build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4220655775"
+    serviceWorkerVersion: "1446031038"
   }
 });
